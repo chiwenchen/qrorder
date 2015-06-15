409a22f5c09afde2d3ddf9568a3f83d6defcf6dd
Chiwen Chen <cwchen2000@gmail.com>
2015-06-15 23:42:26 +0800

    complete basic navbar and photo of menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,60 @@
 asia_food = Restaurant.create(name: 'Asian Food')
 salad = Restaurant.create(name: 'Happy Salad')
 
-thai_fish = Menu.create(
-  dish_name: 'Thai Fish', 
-  description: 'very spicy fish', 
+chicken_with_cashew_nuts = Menu.create(
+  dish_name: 'Chicken with Cashew Nuts', 
+  description: 'some description',
+  photo: '/img/chicken-with-cashew-nuts.jpg', 
   restaurant: asia_food)
 
-fried_shrimp = Menu.create(
-  dish_name: 'Fried Shrimp', 
-  description: 'very crippy shrimp', 
+green_chicken_curry = Menu.create(
+  dish_name: 'Green Chicken Curry', 
+  description: 'some description',
+  photo: '/img/green-chicken-curry.jpg', 
   restaurant: asia_food)
+
+khao_pad = Menu.create(
+  dish_name: 'Khao Pad', 
+  description: 'some description',
+  photo: '/img/khao-pad.jpg', 
+  restaurant: asia_food)
+
+pad_pak_boong = Menu.create(
+  dish_name: 'Pad Pak Boong', 
+  description: 'some description',
+  photo: '/img/pad-pak-boong.jpg', 
+  restaurant: asia_food)
+
+pad_thai = Menu.create(
+  dish_name: 'Pad Thai', 
+  description: 'some description',
+  photo: '/img/pad-thai.jpg', 
+  restaurant: asia_food)
+
+red_curry = Menu.create(
+  dish_name: 'Red Curry', 
+  description: 'some description',
+  photo: '/img/red-curry.jpg', 
+  restaurant: asia_food)
+
+somtam = Menu.create(
+  dish_name: 'Somtam', 
+  description: 'some description',
+  photo: '/img/somtam.jpg', 
+  restaurant: asia_food)
+
+spicy_beef_salad = Menu.create(
+  dish_name: 'Spicy Beef Salad', 
+  description: 'some description',
+  photo: '/img/spicy-beef-salad.jpg', 
+  restaurant: asia_food)
+
+tom_kha_kai = Menu.create(
+  dish_name: 'Tom Kha Kai', 
+  description: 'some description',
+  photo: '/img/tom-kha-kai.jpg', 
+  restaurant: asia_food)
+
 
 veg_salad = Menu.create(
   dish_name: 'Vegetarian Salad', 
